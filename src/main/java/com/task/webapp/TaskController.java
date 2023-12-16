cec7da9c02c19f26691cc372f7a758c680481074
Enock97 <enockladu@hotmail.com>
2023-12-16 21:23:48 +0100

    Edited files with additions, removals and revisions.

--- src/main/java/com/task/webapp/TaskController.java
+++ src/main/java/com/task/webapp/TaskController.java
@@ -1,6 +1,8 @@
 package com.task.webapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,43 +13,60 @@ public class TaskController {
     @Autowired
     TaskRepository rep;
 
-    @PostMapping("/save")
-    public void saveTask(Task task){
-        rep.saveTask(task);
+    // Create a new task
+    @PostMapping("/postTask")
+    public ResponseEntity<String> createTask(@RequestBody Task task) {
+        try {
+            rep.saveTask(task);
+            return ResponseEntity.ok("OK");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error in saving task");
+        }
     }
-    @GetMapping("/getAll")
-    public List<Task> getAll(){
+
+    // Retrieve all tasks
+    @GetMapping("/getAllTasks")
+    public List<Task> getAllTasks() {
         return rep.getTasks();
     }
 
-    @GetMapping("getOneTask/{id}")
-    public Task getTaskById(@PathVariable int id) {
-        return rep.getTaskById(id);
+    // Retrieve a task by ID
+    @GetMapping("/getSingleTask/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable int id) {
+        Task task = rep.getTaskById(id);
+        if (task != null) {
+            return ResponseEntity.ok(task);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    @PutMapping("/update/{id}")
-    public void updateTask(@PathVariable int id, Task updatedTask) {
+    // Update a task by ID
+    @PutMapping("/updateTask/{id}")
+    public ResponseEntity<String> updateTask(@PathVariable int id, @RequestBody Task updatedTask) {
         Task existingTask = rep.getTaskById(id);
 
         if (existingTask != null) {
             existingTask.setName(updatedTask.getName());
             existingTask.setDescription(updatedTask.getDescription());
             rep.updateTask(existingTask);
+            return ResponseEntity.ok("OK");
+        } else {
+            return ResponseEntity.notFound().build();
         }
     }
 
-    @DeleteMapping("/delete/{id}")
-    public void deleteTask(@PathVariable int id) {
+    // Delete a task by ID
+    @DeleteMapping("/deleteTask/{id}")
+    public ResponseEntity<String> deleteTask(@PathVariable int id) {
         Task existingTask = rep.getTaskById(id);
 
         if (existingTask != null) {
             rep.deleteTaskById(id);
+            return ResponseEntity.ok("OK");
+        } else {
+            return ResponseEntity.notFound().build();
         }
     }
-
-    @DeleteMapping("/deleteAll")
-    public void deleteAll(){
-        rep.deleteAllTasks();
-    }
-
 }
+
--- src/main/resources/static/task-main-page.js
+++ src/main/resources/static/task-main-page.js
@@ -2,23 +2,8 @@ $(function(){
     getAll();
 });
 
-/*
-function createTask() {
-    const task = {
-        Name : $("#Name").val(),
-        Description : $("#Description").val()
-    };
-    const url = "/save";
-    $.post(url, task, function(result){
-        getAll();
-    });
-    $("#Name").val("");
-    $("#Description").val("");
-};
-*/
-
 function getAll() {
-    $.get( "/getAll", function( data ) {
+    $.get( "/getAllTasks", function( data ) {
         formatData(data);
     });
 };
@@ -30,14 +15,22 @@ function formatData(tasks){
         "</tr>";
     for (let i in tasks) {
         out += "<tr><td>"+tasks[i].name+"<br>"+tasks[i].description+"</td>" +
-               "<td><a href='../edit-task/edit-task.html'><button class='btn btn-success'>Edit</button></a></td>" +
+               "<td><a href='edit-task.html?id="+tasks[i].id+"'><button class='btn btn-success'>Edit</button></a></td>" +
                "<td><button class='btn btn-danger' onclick='deleteTask("+tasks[i].id+")'>Delete</button></td></tr>";
     }
     $("#tasks").html(out);
 }
 
 function deleteTask(taskId) {
-    $.get("/delete/" + taskId, function(data) {
-        getAll(); // Refresh the task list after deleting
+    $.ajax({
+        url: "/deleteTask/" + taskId,
+        type: "DELETE",
+        success: function (response) {
+            getAll();
+        },
+        error: function (xhr, status, error) {
+            console.error("Error deleting task: " + error);
+        }
     });
 }
+
