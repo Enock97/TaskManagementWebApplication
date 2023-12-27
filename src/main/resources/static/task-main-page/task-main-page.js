347d6624f6c87c32268329047b7c10e1a7dbf957
Enock97 <enockladu@hotmail.com>
2023-12-27 19:06:47 +0100

    Changed coloring of task list div and buttons.

--- src/main/resources/static/task-main-page/task-main-page.js
+++ src/main/resources/static/task-main-page/task-main-page.js
@@ -9,13 +9,13 @@ function getAll() {
 };
 
 function formatData(tasks){
-    var out = "<table class='table table-bordered table-hover table-info'>" +
+    var out = "<table class='table table-bordered table-hover table-light'>" +
         "<tr>" +
         "<th style='font-weight: bold;'>Task</th><th>Edit Tasks</th><th>Complete Tasks</th>" +
         "</tr>";
     for (let i in tasks) {
         out += "<tr><td>"+tasks[i].name+"<br>"+tasks[i].description+"</td>" +
-               "<td><a href='../edit-task/edit-task.html?id="+tasks[i].id+"'><button class='btn btn-light'>Edit</button></a></td>" +
+               "<td><a href='../edit-task/edit-task.html?id="+tasks[i].id+"'><button class='btn btn-secondary'>Edit</button></a></td>" +
                "<td><button class='btn btn-success' onclick='deleteTask("+tasks[i].id+")'>Complete</button></td></tr>";
     }
     $("#tasks").html(out);
