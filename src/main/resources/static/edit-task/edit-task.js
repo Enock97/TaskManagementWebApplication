91a888ee3945bffb06cee969f369c9d67d469279
Enock97 <enockladu@hotmail.com>
2023-12-27 18:54:36 +0100

    Added simple error handling

--- src/main/resources/static/edit-task/edit-task.js
+++ src/main/resources/static/edit-task/edit-task.js
@@ -25,16 +25,49 @@ function loadTaskData(taskId) {
 
 function updateTask() {
     const taskId = $('#taskId').val();
-    const task = {
-        name: $("#Name").val(),
-        description: $("#Description").val()
-    };
+    const name = $("#Name").val();
+    const description = $("#Description").val();
 
+    // Regex for name and description validation
+    const nameRegex = /[0-9a-zA-ZøæåØÆÅ:_@#\/%&,(). -]{2,30}/;
+    const descriptionRegex = /[0-9a-zA-ZøæåØÆÅ:_@#!\/%&,(). -]{10,1000}/;
+
+    let hasError = false;
+
+    // Clear previous errors
+    $("#feil1").html('');
+    $("#feil2").html('');
+    $("#feil3").html('');
+
+    // Check if name or description is empty
+    if (!name || !description) {
+        $("#feil3").html("Error in input - missing name or description");
+        hasError = true;
+    }
+
+    // Check if name doesn't match the regex
+    if (!nameRegex.test(name)) {
+        $("#feil1").html("Error: Invalid name format");
+        hasError = true;
+    }
+
+    // Check if description doesn't match the regex
+    if (!descriptionRegex.test(description)) {
+        $("#feil2").html("Error: Invalid description format");
+        hasError = true;
+    }
+
+    // If there is any error, stop the function execution
+    if (hasError) {
+        return;
+    }
+
+    // If all checks pass, proceed with AJAX
     $.ajax({
         url: "/updateTask/" + taskId,
         type: "PUT",
         contentType: "application/json",
-        data: JSON.stringify(task),
+        data: JSON.stringify({ name, description }),
         success: function (response) {
             window.location.href = '../task-main-page/task-main-page.html';
         },
@@ -43,6 +76,7 @@ function updateTask() {
         }
     });
 }
+
 
 
 
